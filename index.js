7804008f4c47e4ae42cdd2f5a86654f5cabf4461
Shadow <william.shadow@outlook.com>
2021-10-12 16:43:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
 const app = new express()
 app.use(express.static('files'))
-app.listen(process.env.PORT || 3000).then(console.log(`Site is online, running on port ${process.env.PORT || 3000}`))
+app.listen(process.env.PORT || 3000)
+console.log(`Site is online, running on port ${process.env.PORT || 3000}`)
